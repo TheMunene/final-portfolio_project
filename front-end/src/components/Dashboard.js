f0ba01d085c31979c2e0479e0ad3900abf10a1f7
Josphat munene <munenejosphat17@gmail.com>
2024-08-21 15:01:20 +0300

    loggedin user dashboard

---
+++ front-end/src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Dashboard = () => {
+    return(
+        <div className="mt-20 text-3xl text-black font-normal">
+            welcome back!
+        </div>
+    );
+}
+export default Dashboard;
