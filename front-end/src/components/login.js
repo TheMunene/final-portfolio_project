9b23adb237f74a91dffacfd7a72c7bce06fd5787
Josphat munene <munenejosphat17@gmail.com>
2024-08-21 15:02:43 +0300

    user login page

---
+++ front-end/src/components/login.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase"; // Import the Firebase auth instance
+import logo from "../assets/logo.png";
+
+const Login = ({ login }) => { // Accept the login function as a prop
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            login(); // Call the login function to update the state in App.js
+            alert("Login successful!");
+            navigate('/'); // Redirect to homepage
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
+    return (
+        <div className="flex flex-1 flex-col bg-slate-100 justify-center mt-12 px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img alt="Your Company" src={logo} className="mx-auto h-20 w-auto" />
+                <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                    Sign in to your account
+                </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form onSubmit={handleLogin} className="space-y-6">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                            Email address
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                autoComplete="email"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                            Password
+                        </label>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                autoComplete="current-password"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                    Not a member?{" "}
+                    <a href="/signUp" className="font-semibold leading-6 text-orange-500 hover:text-black">
+                        Sign up to get an account
+                    </a>
+                </p>
+            </div>
+            <div className="border border-gray-400 mt-8"></div>
+            <span className="text-sm mt-5 text-gray-400 text-center block">
+                ©<a href="">Empie Dance Kenya</a> 2024, All rights reserved.
+            </span>
+        </div>
+    );
+};
+
+export default Login;
